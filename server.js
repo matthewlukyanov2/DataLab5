6eea26b85a217c08f385afe762f157face20b861
unknown <brucelukyanov@gmail.com>
2024-10-25 10:55:28 +0000

    Excercise 5

--- server.js
+++ server.js
@@ -20,6 +20,33 @@ app.get('/hello/:name/:surname', (req, res) => {
     res.send('Hello ' +req.params.name+ " " +req.params.surname); 
 });
 
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(200).json({ myMovies:movies });
+});
+
 // Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
